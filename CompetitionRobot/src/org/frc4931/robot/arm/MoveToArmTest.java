8c4daeaa24fd53c99558ca670b98b0fcdf1035ef
Julian Nieto <nieto225@gmail.com>
2016-01-26 19:25:02 -0600

    Test for 'MoveToArm'

---
+++ CompetitionRobot/src/org/frc4931/robot/arm/MoveToArmTest.java
@@ -0,0 +1,17 @@
+package org.frc4931.robot.arm;
+
+import org.junit.Before;
+import org.strongback.mock.Mock; 
+
+public class MoveToArmTest 
+{
+	private MoveArmTo x;
+	private Mock t;
+	private Arm a;
+	@Before
+	public void beforeEach()
+	{
+		t=new Mock();
+		x=new Move
+	}
+}
